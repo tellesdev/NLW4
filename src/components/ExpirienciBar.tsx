4ed108c9a94a062b3c5abbdae5c09349847c66df
Telles <telles@MacBook-Air-de-Alex.local>
2021-02-22 22:59:13 -0300

    Iniciando projeto NLW4

---
+++ src/components/ExpirienciBar.tsx
@@ -0,0 +1,17 @@
+export function ExpirienciBar() {
+  return (
+    <header className="experience-bar">
+      <span >0 xp</span>
+      <div>
+        <div style={{ width: '50%'}}>
+
+          <span className="current-experience" style={{ left: '50%'}}>300 xp</span>
+
+        </div>
+
+      </div>
+
+      <span>600 xp</span>
+    </header>
+  )
+}
